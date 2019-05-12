791b064a11e46e9674c12b2392b5f69f4125e614
ccwang <abelardccwang@gmail.com>
2019-05-13 08:47:07 +0900

    updated Chapter02

--- Chapter02.R
+++ Chapter02.R
@@ -31,3 +31,12 @@ cov2cor(margCov.int[[1]])
 lmeFit.slp <- lme(CD4 ~ obstime, random = ~ obstime | patient, data = aids)
 
 summary(lmeFit.slp)
+
+
+# Page 27 -----------------------------------------------------------------
+
+margCov.slp <- getVarCov(lmeFit.slp, individuals = 12, type = "marginal")
+
+margCov.slp
+
+cov2cor(margCov.slp[[1]])
