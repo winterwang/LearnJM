faaf835f602403d330708a783dd0a8957ffb9a89
C.Wang <abelardccwang@gmail.com>
2019-05-17 14:14:11 +0900

    updated with gif and animation of hazard prediction

--- Chapter04.R
+++ Chapter04.R
@@ -21,3 +21,31 @@ coxFit.aids <- coxph(Surv(Time, death) ~ drug, data = aids.id, x = TRUE)
 jointFit.aids <- jointModel(lmeFit.aids, coxFit.aids, timeVar = "obstime", 
                             method = "piecewise-PH-aGH")
 summary(jointFit.aids)
+
+# dynamic prediction GIF
+
+
+# We are interested in producing predictions of survival probabilities for Patient 155
+dataP110 <- aids[aids$patient == 3, ]
+len_id <- nrow(dataP110)
+
+
+# We can plot the data
+sfit3 <- survfitJM(jointFit.aids, idVar = "patient",  newdata = dataP110[1:2, ]) 
+sfit4 <- survfitJM(jointFit.aids, idVar = "patient",  newdata = dataP110[1:3, ]) 
+
+
+
+par(mfrow=c(1,2))
+plotfit3 <- plot(sfit3, estimator="mean", include.y = TRUE, conf.int=0.95, fill.area=TRUE, col.area="lightblue", main="Patient 10")
+plotfit4 <- plot(sfit4, estimator="mean", include.y = TRUE, conf.int=0.95, fill.area=TRUE, col.area="lightblue", main="Patient 10")
+
+
+library(animation)
+saveGIF({
+  for(i in c(1:len_id)){
+    sfit <- survfitJM(jointFit.aids, idVar = "patient", newdata = dataP110[1:i, ]) 
+    plot(sfit, estimator="mean", include.y = TRUE, conf.int=0.95, fill.area=TRUE, col.area="lightblue", main="Patient 3")
+    
+  }
+},ani.width = 400, ani.height=400)
